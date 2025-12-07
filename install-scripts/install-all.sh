339887ff8775350babefa033a4e5302aca886a8b
rw42 <roguewizard42@gmail.com>
2025-12-06 21:02:40 -0500

    Add master install script

---
+++ install-scripts/install-all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install all packages
+
+./install-omarchypkg.sh
+./install-virtpkg.sh
+./install-gamepkg.sh
+./install-dotfiles.sh
+./install-hyprland-overrides.sh
+./install-bindings-overrides.sh
+
+
